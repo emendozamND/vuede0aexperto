2a89714c71d0c1e6feb9724a5c49c6dbe599870b
Elias Mendoza <eliasmm@gmail.com>
2025-12-13 08:39:21 -0600

    forma extendida del composition API

--- codersfree/src/App.vue
+++ codersfree/src/App.vue
@@ -1,5 +1,13 @@
-<script setup>
+<script >
 const msg = "Hola mundo";
+export default{
+    setup(){
+        const msg = 'Hola mundo';
+        return {
+            msg
+        }
+    }
+}
 </script>
 <template>
 <h1>{{ msg }}</h1>
