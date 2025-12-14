8feb52d5b56dc6efa97ba0ae6add530204486c28
Elias Mendoza <eliasmm@gmail.com>
2025-12-14 02:39:55 -0600

    add conditional rendering demo (v-if / v-else-if)

--- codersfree/src/App.vue
+++ codersfree/src/App.vue
@@ -1,17 +1,29 @@
 <script setup>
-import {ref } from 'vue';
-const awesome = ref(true);
+import { ref } from "vue";
+
+const type = ref("a");
+const mostrar = ref(true);
 </script>
 
 <template>
-  <h1 v-if="awesome">
-  Vue es increible  
-  </h1>
-    <h1 v-else>
-  Vue no es increible  
-  </h1>
+  <button @click="type = 'a'">A</button>
+  <button @click="type = 'b'">B</button>
+  <button @click="type = 'c'">C</button>
+  <button @click="mostrar = !mostrar">Mostrar</button>
 
+  <!-- Cadena IF -->
+  <h1 v-if="type === 'a'">A</h1>
+  <h2 v-else-if="type === 'b'">B</h2>
+  <h3 v-else-if="type === 'c'">C</h3>
+  <p v-else>No es A, B ni C</p>
+
+  <!-- Otro IF independiente -->
+  <div v-if="mostrar">
+    <h1>Hola Mundo</h1>
+    <h2>parrafo 2</h2>
+    <h3>parrafo 3</h3>
+  </div>
+  <div v-else>
+    <p>is hide</p>
+  </div>
 </template>
-<style scoped>
-
-</style>
