7a49055673ac48fc4db6640d53f0bd4b8d1679c8
Elias Mendoza <eliasmm@gmail.com>
2025-12-13 23:04:39 -0600

    cambiar estilo con ref

--- codersfree/src/App.vue
+++ codersfree/src/App.vue
@@ -1,32 +1,17 @@
 <script setup>
-import { reactive, computed } from "vue";
-
-const user = reactive({
-  name: "Elias",
-  apellido: "Mendoza",
-  edad: 16,
-});
-
-const estado = computed(() => {
-  return user.edad >= 18 ? "Eres mayor de Edad" : "Eres menor de Edad";
-});
-
-const increment = () => {
-  user.edad++;
-  console.log(user.edad);
-};
-
-const decrement = () => {
-  user.edad--;
-  console.log(user.edad);
-};
+import {ref, computed} from 'vue';
+const pintar = ref(false);
 </script>
 
 <template>
-  <div>
-    <button @click="decrement">-</button>
-    <span>{{ user.edad }}</span>
-    <button @click="increment">+</button>
-    <h1>{{ estado }}</h1>
-  </div>
+ <button @click="pintar = !pintar">
+  Pintar
+ </button>
+ <h1 :class="{ 'bg-red':pintar
+ }">Hola mundo</h1>
 </template>
+<style scoped>
+.bg-red{
+  background-color:red;
+}
+</style>
