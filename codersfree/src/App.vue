019ab35f9e05448f1b6b3572dc80a4938c727dda
Elias Mendoza <eliasmm@gmail.com>
2025-12-14 02:54:21 -0600

    adding background  folder

--- codersfree/src/App.vue
+++ codersfree/src/App.vue
@@ -4,6 +4,19 @@ import { ref } from "vue";
 const type = ref("a");
 const mostrar = ref(true);
 </script>
+<script setup>
+import { ref, computed } from "vue";
+
+const type = ref("a");
+const mostrar = ref(true);
+
+const titulo = computed(() => {
+  if (type.value === "a") return "A";
+  if (type.value === "b") return "B";
+  if (type.value === "c") return "C";
+  return "Selecciona A, B o C";
+});
+</script>
 
 <template>
   <button @click="type = 'a'">A</button>
@@ -11,19 +24,15 @@ const mostrar = ref(true);
   <button @click="type = 'c'">C</button>
   <button @click="mostrar = !mostrar">Mostrar</button>
 
-  <!-- Cadena IF -->
-  <h1 v-if="type === 'a'">A</h1>
-  <h2 v-else-if="type === 'b'">B</h2>
-  <h3 v-else-if="type === 'c'">C</h3>
-  <p v-else>No es A, B ni C</p>
+  <h1>{{ titulo }}</h1>
 
-  <!-- Otro IF independiente -->
   <div v-if="mostrar">
     <h1>Hola Mundo</h1>
     <h2>parrafo 2</h2>
     <h3>parrafo 3</h3>
   </div>
   <div v-else>
-    <p>is hide</p>
+    <p>Está oculto</p>
   </div>
 </template>
+
