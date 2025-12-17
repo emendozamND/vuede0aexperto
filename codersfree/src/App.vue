bf5e682db08505c0f3332acb9cee34ece4337629
Elias Mendoza <eliasmm@gmail.com>
2025-12-16 23:08:41 -0600

    eventos y propps comunicación bidireccional

--- codersfree/src/App.vue
+++ codersfree/src/App.vue
@@ -1,17 +1,29 @@
 <script setup>
-  import DetailCourse from './components/DetailCourse.vue';
- const courses = [
+import { ref } from "vue";
+import DetailCourse from "./components/DetailCourse.vue";
+
+const courses = [
   { title: "Vue desde cero", professor: "Victor Arana", price: 10, description: "Aprende Vue desde cero y crea aplicaciones" },
   { title: "Angular desde cero", professor: "Victor Arana", price: 10, description: "Aprende Angular desde cero y crea aplicaciones" },
   { title: "React desde cero", professor: "Victor Arana", price: 10, description: "Aprende React desde cero y crea aplicaciones" }
 ];
+
+const count = ref(5);
+function decrementCount(n){
+  count.value -= n;
+}
+
+const decrement = () => {
+  if (count.value > 0) count.value--;
+};
 </script>
 
 <template>
-  <DetailCourse :courses="courses" prueba="hola mundo"/>
-  
+  <input type="text" v-model.number="count" />
+
+  <DetailCourse
+    :courses="courses"
+    :count="count"
+    @decrement-count="decrementCount"
+  />
 </template>
-
-<style scoped>
-
-</style>
--- codersfree/src/components/DetailCourse.vue
+++ codersfree/src/components/DetailCourse.vue
@@ -1,23 +1,23 @@
 <script setup>
 const props = defineProps({
-  courses: Array,
-    prueba: String
+  courses: { type: Array, required: true },
+  count: { type: Number, default: 0 }
 });
+
+const emit = defineEmits(["decrementCount"]);
 </script>
 
 <template>
   <div>
-    <div v-for="(c, i) in courses" :key="i">
-      <h1>{{ c.title }}</h1>
+    <div v-for="(c, i) in props.courses" :key="i">
+      <h1>{{ c.title }} ({{ props.count }})</h1>
       <p>{{ c.professor }}</p>
       <p>{{ c.price }}</p>
       <p>{{ c.description }}</p>
     </div>
+
+    <button @click="emit('decrementCount',2)">
+      Hazme click
+    </button>
   </div>
-  <p>{{ props.prueba }}</p>
 </template>
-
-
-<style scoped>
-
-</style>
